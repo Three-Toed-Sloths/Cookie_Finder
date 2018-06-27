f209866fab1ceb924dd406ddf88e220dd0efb073
Nick Clear <nickclear22@gmail.com>
2018-06-27 10:48:46 -0700

    a

--- models/seller.js
+++ models/seller.js
@@ -32,14 +32,14 @@ module.exports = function(sequelize, DataTypes) {
             }
         },
         createdAt: {
-                allowNull: false,
-                type: DataTypes.DATE,
-                defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+            allowNull: false,
+            type: DataTypes.DATE,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
         },
         updatedAt: {
-                allowNull: false,
-                type: DataTypes.DATE,
-                defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+            allowNull: false,
+            type: DataTypes.DATE,
+            defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
         }
     });
 
