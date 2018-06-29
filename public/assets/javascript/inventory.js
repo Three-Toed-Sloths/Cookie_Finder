6d09e83c13e04c8d34a51a7a28823aabcb7e9052
Marco Saglietto <saglietto_marco@yahoo.com>
2018-06-29 10:58:24 -0700

    worked on inventory form

--- public/assets/javascript/inventory.js
+++ public/assets/javascript/inventory.js
@@ -1,5 +1,9 @@
-$(document).ready(function () {
 
+
+
+
+$(document).ready(function () {
+    var noUiSlider = require("nouislider");
     // adds a range bar for checked cookies and removes range bar for unchecked cookies
     
     $("#cookies-selector div ul li").on("click", function(){ 
@@ -8,12 +12,22 @@ $(document).ready(function () {
         let selectedCookie = $(this).find("span label span").text();  
         let cookieID = $(this).attr("id");
         let cookieQuantity = $()
-        let rangeBar = `<form action="#" id=1${cookieID}><p class="range-field">Quantity of ${selectedCookie}<input type="range" min="1" max="50" value="1" /></p></form>`;
-              
+        // let rangeBar = `<form action="#" id=1${cookieID}><p class="range-field">Quantity of ${selectedCookie}<input type="range" min="1" max="50" value="1" /></p></form>`;
+        let rangeBar = `<div id="test-slider"></div>`;
+         var rangeB = $("#test-slider");
+         noUiSlider.create(rangeB, {
+             start: 40,
+             connect: "lower",
+             range: {
+                 min: 0,
+                 max: 100
+             }
+         }); 
+        
         
 
         if ( $(this).attr("class") === "selected" ){
-
+            console.log(selectedCookie);            
             $(rangeBar).appendTo("#Cookie-Quantity");
         }else {
             $(`#1${cookieID}`).remove();
